b1df0ea953e8759c72a90e247e1538c043cc767c
Victor Chavarro <info@vianch.com>
2015-09-17 15:14:53 -0500

    web pack angular with template
    web pack angular with template

--- webpack-angular/webpack.config.js
+++ webpack-angular/webpack.config.js
@@ -21,6 +21,11 @@ module.exports = {
 				test: /\.js$/,
 				exclude: /node_modules/,
 				loader: "babel-loader"
+			},
+			{
+				test: /\.html$/,
+				exclude: /node_modules/,
+				loader: "raw-loader"
 			}
 		]
 	},
