4e98048d5392e86b1ac7db5d441bf3e10c5b37cc
Victor Chavarro <info@vianch.com>
2015-09-17 11:33:02 -0500

    use of plugins with web pack
    use of plugins with web pack

--- js/home_page.js
+++ js/home_page.js
@@ -3,6 +3,8 @@ var config = require('../loader/config.json');
 console.log("HOME LOADED!"); 
 console.log(config.app_load_message);
 
+$("#initial_content").text("JQUERY modified this content see(home_page.js)")
+
 var img = document.createElement('img');
 img.style.height = "100%";
 img.style.width = "100%";
